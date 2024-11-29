54e08e15c66e530221b4fbb4e22273a778ee7b8a
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-29 14:48:35 +0100

    total tailwind stup

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,31 +4,27 @@ export default {
   theme: {
     extend: {
       fontSize: {
-        "text-14": "1.4rem"
+        "text-14": "1.4rem",
         "text-16": "1.6rem",
         "text-18": "1.8rem",
-        "text-20": "2rem",
+        "text-20": "2.0rem",
+        "text-22": "2.2rem",
         "text-24": "2.4rem",
+        "text-26": "2.6rem",
         "text-28": "2.8rem",
+        "text-30": "3.0rem",
         "text-32": "3.2rem",
+        "text-34": "3.4rem",
         "text-36": "3.6rem",
-        "text-40": "4rem",
-        "text-44": "4.4rem",
-        "text-48": "4.8rem",
-        "text-52": "5.2rem",
-        "text-56": "5.6rem",
-        "text-60": "6rem",
-        "text-64": "6.4rem",
-        "text-72": "7.2rem",
-        "text-80": "8rem",
-        "text-96": "9.6rem",
+        "text-38": "3.8rem",
+        "text-40": "4.0rem",
       },
       colors: {
         lemon: {
           default: "#e6f2d3",
           dark: "#d3e6b5",
-        }
-      }
+        },
+      },
     },
   },
   plugins: [],
