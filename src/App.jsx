9f9fe14a32ba58fe2341427d05d73b3b0c4f48b1
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-29 12:33:52 +0100

    initial project route in app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Syntheos</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
