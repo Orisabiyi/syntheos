5da36d8a6356422fc2868b3a83e7c99bdca3344f
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-29 15:08:57 +0100

    style: style the Auth page component

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,17 +1,23 @@
 export default function Auth() {
   return (
-    <main>
+    <main className="min-h-screen flex flex-col bg-lemon-default">
       <nav>
         <h1>Syntheos</h1>
       </nav>
 
-      <section className="flex flex-col items-center justify-center">
+      <section className="flex flex-col items-center justify-center flex-1">
         <figure></figure>
 
-        <div className="flex flex-col items-center justify-center">
-          <h1>Welcome to Syntheos</h1>
-          <button>Sign in/Sign up with Google</button>
-          <button>Sign in/Sign up X</button>
+        <div className="flex flex-col items-center justify-center gap-4">
+          <h1 className="text-32 font-semibold">Welcome to Syntheos</h1>
+
+          <button className="bg-lemon-dark text-16 px-8 py-4 w-72 rounded-xl">
+            Join with Google
+          </button>
+
+          <button className="bg-lemon-dark text-16 px-8 py-4 w-72 rounded-xl">
+            Join with X
+          </button>
         </div>
       </section>
     </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,20 +4,18 @@ export default {
   theme: {
     extend: {
       fontSize: {
-        "text-14": "1.4rem",
-        "text-16": "1.6rem",
-        "text-18": "1.8rem",
-        "text-20": "2.0rem",
-        "text-22": "2.2rem",
-        "text-24": "2.4rem",
-        "text-26": "2.6rem",
-        "text-28": "2.8rem",
-        "text-30": "3.0rem",
-        "text-32": "3.2rem",
-        "text-34": "3.4rem",
-        "text-36": "3.6rem",
-        "text-38": "3.8rem",
-        "text-40": "4.0rem",
+        14: "1.4rem",
+        16: "1.6rem",
+        18: "1.8rem",
+        20: "2.0rem",
+        22: "2.2rem",
+        24: "2.4rem",
+        28: "2.8rem",
+        32: "3.2rem",
+        34: "3.4rem",
+        36: "3.6rem",
+        38: "3.8rem",
+        40: "4.0rem",
       },
       colors: {
         lemon: {
