c5d379777970d993f69207aa7d34c6c4d51979f5
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-29 17:50:51 +0100

    feat: setup the function to handleTwitterAuth

---
+++ src/firebase/TwitterAuth.js
@@ -0,0 +1,16 @@
+import { TwitterAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { app } from "./firebaseConfig";
+
+export async function handleTwitterAuth() {
+  const provider = new TwitterAuthProvider();
+  const auth = getAuth(app);
+
+  try {
+    const result = await signInWithPopup(auth, provider);
+    const credential = TwitterAuthProvider.credentialFromResult(result);
+    const user = result.user;
+    return { user, credential };
+  } catch (error) {
+    console.log(error.message);
+  }
+}
