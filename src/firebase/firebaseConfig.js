0e530f94d54d28132af7ae040b099cf6b3df24a5
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-29 13:33:47 +0100

    chore: setup firebase config

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBdpUiNUOlmTX3XsIp3BH9h_qHivxumxV0",
+  authDomain: "syntheos-c76fa.firebaseapp.com",
+  projectId: "syntheos-c76fa",
+  storageBucket: "syntheos-c76fa.firebasestorage.app",
+  messagingSenderId: "666874661819",
+  appId: "1:666874661819:web:52e066000880ef2194d29e",
+  measurementId: "G-S78FRS44MX",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
