d796c7178846e3067425e38c23af0b44bae907c4
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-29 12:44:41 +0100

    chore: initialize pages component and complete basic route setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Auth from "./pages/Auth";
+import Dashboard from "./pages/Dashboard";
+import Analyze from "./pages/Analyze";
+import Profile from "./pages/Profile";
+import History from "./pages/History";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Syntheos</h1>} />
+        <Route path="/" element={<Auth />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="analyze" element={<Analyze />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="history" element={<History />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
