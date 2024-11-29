3ac890fc05563157d916889ab81fffce9583e2d8
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-29 14:17:06 +0100

    feat: setup the auth skeleton

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,1 +1,15 @@
-export default function Auth() {}
+export default function Auth() {
+  return (
+    <main>
+      <nav>
+        <h1>Syntheos</h1>
+      </nav>
+
+      <section>
+        <figure></figure>
+        <button>Sign in/Sign up with Google</button>
+        <button>Sign in/Sign up X</button>
+      </section>
+    </main>
+  );
+}
