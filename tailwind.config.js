c6261c087094fb28fc47b0bbe0c27ae5438a613e
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-29 12:25:25 +0100

    style: create custom font size in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        "text-14": "1.4rem",
+        "text-16": "1.6rem",
+        "text-18": "1.8rem",
+        "text-20": "2rem",
+        "text-24": "2.4rem",
+        "text-28": "2.8rem",
+        "text-32": "3.2rem",
+        "text-36": "3.6rem",
+        "text-40": "4rem",
+        "text-44": "4.4rem",
+        "text-48": "4.8rem",
+        "text-52": "5.2rem",
+        "text-56": "5.6rem",
+        "text-60": "6rem",
+      },
+    },
   },
   plugins: [],
 };
