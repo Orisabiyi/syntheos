188058e226984829193e96676b9875da29586d46
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-30 18:56:17 +0100

    style: update the height screen

--- src/pages/Analyze.jsx
+++ src/pages/Analyze.jsx
@@ -1,21 +1,24 @@
 import { useNavigate } from "react-router-dom";
 
 export default function Analyze() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handlePasteClick = () => {
-    navigate("/dashboard/analyzer")
-  }
+    navigate("/dashboard/analyzer");
+  };
   return (
-    <div className="w-full h-full py-2 px-2">
-      <div className="flex flex-row justify-center h-full items-center gap-10">
+    <div className="w-full min-h-screen py-2 px-2">
+      <div className="flex flex-row justify-center min-h-screen items-center gap-10">
         <button className="h-32 bg-lemon-dark w-80 text-black rounded-lg text-20">
           Select from X
         </button>
-        <button className="h-32 border-2 border-black w-80 rounded-lg text-20" onClick={handlePasteClick}>
+        <button
+          className="h-32 border-2 border-black w-80 rounded-lg text-20"
+          onClick={handlePasteClick}
+        >
           Paste content
         </button>
       </div>
     </div>
-  )
+  );
 }
