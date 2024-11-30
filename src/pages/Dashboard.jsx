d2786d1a03cfe8aa6bf8b81d73870a5f46399d62
Thoobi <sydneydaniel76@gmail.com>
2024-11-30 01:17:58 +0100

    chores: adds the analyzer page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,8 +5,8 @@ export default function Dashboard() {
     <main className="max-w-full mx-auto h-screen bg-lemon-default">
       <section className="w-full px-auto h-full">
         <section className="bg-lemon-dark flex flex-row gap-3 h-full">
-          <div className="w-1/4 bg-lemon-dark flex flex-col py-12 px-10">
-            <h1 className="text-30">Syntheos</h1>
+          <div className="w-1/5 bg-lemon-dark flex flex-col py-12 px-10">
+            <h1 className="text-28">Syntheos</h1>
             <nav className="text-18 h-full mt-40 gap-10 rounded-lg flex flex-col">
               <NavLink to={'analyze'} className="py-2 border-b-[1.5px] border-b-black">
                 Analyze
@@ -20,7 +20,7 @@ export default function Dashboard() {
             </nav>
           </div>
 
-          <div className="bg-lemon-default w-11/12 rounded-lg">
+          <div className="bg-lemon-default w-4/5 h-full rounded-lg">
             <Outlet />
           </div>
         </section>
