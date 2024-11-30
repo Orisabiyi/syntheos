3a564f4ad51baafa3361c7dca4a29ae6f171fb6d
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-30 08:30:26 +0100

    chore: include a placeholder text for input text

--- src/utils/gemini.js
+++ src/utils/gemini.js
@@ -1,6 +1,15 @@
 const apiKey = import.meta.env.VITE_API_KEY;
 export const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`;
 
+export const placeholder = `📸 Share your favorite travel photo! 🌄
+
+Whether it's a stunning landscape, a bustling city, or a serene beach, we want to see your best travel shots. Use the hashtag #TravelPhotoChallenge and tag us for a chance to be featured!
+
+🌟 Winner gets a special shoutout and a surprise gift! 🎁
+
+#Travel #Photography #Adventure #Wanderlust #TravelPhotoChallenge
+`;
+
 export function optimizedRequest(content) {
   return {
     contents: [
