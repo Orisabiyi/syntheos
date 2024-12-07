940f5c505bfeb9c627bb021e8533570b5b7076d3
Orisabiyi <orisabiyidavid@gmail.com>
2024-12-07 08:11:43 +0100

    feat: complete landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import { FcGoogle } from "react-icons/fc";
+import { RiTwitterXFill } from "react-icons/ri";
 import { useNavigate } from "react-router-dom";
 import { handleGoogleAuth } from "../firebase/GoogleAuth";
 import { handleTwitterAuth } from "../firebase/TwitterAuth";
@@ -31,32 +33,58 @@ export default function Home() {
   };
 
   return (
-    <main className="min-h-screen flex flex-col bg-lemon-default">
+    <main className="bg-gray-200">
       <nav className="px-16 py-8">
-        <h1 className="text-32 font-semibold">Syntheos</h1>
+        <h1 className="text-32 font-semibold">OPRA</h1>
       </nav>
 
-      <section className="flex flex-col items-center justify-center flex-1">
-        <figure></figure>
+      <header className="flex items-center justify-between h-[90vh] px-16">
+        <div className="w-1/2 flex flex-col gap-10">
+          <h2 className="text-60 leading-tight font-semibold">
+            <span className="hover:bg-white hover:px-3 hover:rotate-90 rounded-lg cursor-pointer transition-all duration-200 delay-100">
+              Optimize
+            </span>{" "}
+            your Social Media{" "}
+            <span className="hover:bg-white hover:px-3 hover:-rotate-45 cursor-pointer transition-all duration-200 delay-100">
+              Content
+            </span>{" "}
+            for Maximum{" "}
+            <span className="hover:bg-white hover:px-3 hover:-rotate-45 cursor-pointer transition-all duration-200 delay-100">
+              Engagement
+            </span>
+          </h2>
 
-        <div className="flex flex-col items-center justify-center gap-8">
-          <h1 className="text-32 font-semibold">Welcome to Syntheos</h1>
+          <p className="text-32 font-medium">
+            Analyze, Improve, and Drive Traffic with Opra&apos;s AI-Powered Post
+            Analyzer
+          </p>
 
-          <button
-            className="bg-lemon-dark text-16 px-8 py-4 w-72 rounded-xl cursor-pointer"
-            onClick={handleAuth}
-          >
-            Join with Google
+          <button className="self-start p-6 w-80 bg-white rounded-lg text-20 font-medium">
+            Get Started
           </button>
 
-          <button
-            className="bg-lemon-dark text-16 px-8 py-4 w-72 rounded-xl cursor-pointer"
-            onClick={handleTwitter}
-          >
-            Join with X
-          </button>
+          {/* <ul className="flex gap-10">
+            <li>
+              <button
+                className="flex items-center gap-2 text-20"
+                onClick={handleAuth}
+              >
+                Join with <FcGoogle size={32} />
+              </button>
+            </li>
+            <li>
+              <button
+                className="flex items-center gap-2 text-20"
+                onClick={handleTwitter}
+              >
+                Join with <RiTwitterXFill size={32} />
+              </button>
+            </li>
+          </ul> */}
         </div>
-      </section>
+
+        <div></div>
+      </header>
     </main>
   );
 }
