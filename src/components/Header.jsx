ffe474aa291167e1a9c2f3a5d563236d55560fb7
Orisabiyi <orisabiyidavid@gmail.com>
2024-12-10 06:41:52 +0100

    refactor: create a reusable component for header

---
+++ src/components/Header.jsx
@@ -0,0 +1,55 @@
+import social from "../assets/social.jpeg";
+
+export default function Header() {
+  return (
+    <header className="flex items-center justify-between h-[90vh] p-16 gap-20 bg-gradient-to-b from-gray-100 to-gray-300">
+      <div className="w-1/2 flex flex-col gap-10">
+        <h2 className="text-60 leading-tight font-semibold">
+          <span className="hover:bg-white hover:px-3 hover:rotate-90 rounded-lg cursor-pointer transition-all duration-200 delay-100">
+            Optimize
+          </span>{" "}
+          your Social Media{" "}
+          <span className="hover:bg-white hover:px-3 hover:-rotate-45 cursor-pointer transition-all duration-200 delay-100">
+            Content
+          </span>{" "}
+          for Maximum{" "}
+          <span className="hover:bg-white hover:px-3 hover:-rotate-45 cursor-pointer transition-all duration-200 delay-100">
+            Engagement
+          </span>
+        </h2>
+
+        <p className="text-32 font-medium">
+          Analyze, Improve, and Drive Traffic with Opra&apos;s AI-Powered Post
+          Analyzer
+        </p>
+
+        <button className="self-start bg-white rounded-lg font-medium p-6 w-80 text-20 transition-all duration-500 hover:bg-gray-100">
+          Get Started
+        </button>
+
+        {/* <ul className="flex gap-10">
+            <li>
+              <button
+                className="flex items-center gap-2 text-20"
+                onClick={handleAuth}
+              >
+                Join with <FcGoogle size={32} />
+              </button>
+            </li>
+            <li>
+              <button
+                className="flex items-center gap-2 text-20"
+                onClick={handleTwitter}
+              >
+                Join with <RiTwitterXFill size={32} />
+              </button>
+            </li>
+          </ul> */}
+      </div>
+
+      <figure className="flex-1">
+        <img src={social} alt="social-img" className="rounded-xl" />
+      </figure>
+    </header>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,36 @@
-import social from "../assets/social.jpeg";
-import { Link, NavLink, useNavigate } from "react-router-dom";
-import { handleGoogleAuth } from "../firebase/GoogleAuth";
-import { handleTwitterAuth } from "../firebase/TwitterAuth";
+import { NavLink } from "react-router-dom";
 
 import Faqs from "../components/Faqs";
 import OpraWork from "../components/OpraWork";
 import ChooseOpra from "../components/ChooseOpra";
 
 export default function Home() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  const handleAuth = async function () {
-    try {
-      const value = await handleGoogleAuth();
-      if (value?.user) {
-        navigate("/dashboard");
-        console.log(value.user.uid);
-        sessionStorage.setItem("userID", value.user.uid);
-      }
-    } catch (error) {
-      console.error("Authentication failed:", error);
-    }
-  };
+  // const handleAuth = async function () {
+  //   try {
+  //     const value = await handleGoogleAuth();
+  //     if (value?.user) {
+  //       navigate("/dashboard");
+  //       console.log(value.user.uid);
+  //       sessionStorage.setItem("userID", value.user.uid);
+  //     }
+  //   } catch (error) {
+  //     console.error("Authentication failed:", error);
+  //   }
+  // };
 
-  const handleTwitter = async function () {
-    try {
-      const value = await handleTwitterAuth();
+  // const handleTwitter = async function () {
+  //   try {
+  //     const value = await handleTwitterAuth();
 
-      if (value?.user) {
-        navigate("/dashboard");
-      }
-    } catch (error) {
-      console.error("Authentication failed:", error);
-    }
-  };
+  //     if (value?.user) {
+  //       navigate("/dashboard");
+  //     }
+  //   } catch (error) {
+  //     console.error("Authentication failed:", error);
+  //   }
+  // };
 
   return (
     <main className="bg-gray-100">
@@ -50,56 +47,6 @@ export default function Home() {
 
         <button>Get Started</button>
       </nav>
-
-      <header className="flex items-center justify-between h-[90vh] p-16 gap-20 bg-gradient-to-b from-gray-100 to-gray-300">
-        <div className="w-1/2 flex flex-col gap-10">
-          <h2 className="text-60 leading-tight font-semibold">
-            <span className="hover:bg-white hover:px-3 hover:rotate-90 rounded-lg cursor-pointer transition-all duration-200 delay-100">
-              Optimize
-            </span>{" "}
-            your Social Media{" "}
-            <span className="hover:bg-white hover:px-3 hover:-rotate-45 cursor-pointer transition-all duration-200 delay-100">
-              Content
-            </span>{" "}
-            for Maximum{" "}
-            <span className="hover:bg-white hover:px-3 hover:-rotate-45 cursor-pointer transition-all duration-200 delay-100">
-              Engagement
-            </span>
-          </h2>
-
-          <p className="text-32 font-medium">
-            Analyze, Improve, and Drive Traffic with Opra&apos;s AI-Powered Post
-            Analyzer
-          </p>
-
-          <button className="self-start bg-white rounded-lg font-medium p-6 w-80 text-20 transition-all duration-500 hover:bg-gray-100">
-            Get Started
-          </button>
-
-          {/* <ul className="flex gap-10">
-            <li>
-              <button
-                className="flex items-center gap-2 text-20"
-                onClick={handleAuth}
-              >
-                Join with <FcGoogle size={32} />
-              </button>
-            </li>
-            <li>
-              <button
-                className="flex items-center gap-2 text-20"
-                onClick={handleTwitter}
-              >
-                Join with <RiTwitterXFill size={32} />
-              </button>
-            </li>
-          </ul> */}
-        </div>
-
-        <figure className="flex-1">
-          <img src={social} alt="social-img" className="rounded-xl" />
-        </figure>
-      </header>
 
       <ChooseOpra />
 
