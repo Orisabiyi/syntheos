598736043c1093f39d1d610caa442c6ec9a0f050
Orisabiyi <orisabiyidavid@gmail.com>
2024-12-11 07:30:25 +0100

    style: remove unused code and optimize it for different screen sizes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 export default function Button() {
   return (
-    <button className="self-start bg-white rounded-lg font-medium py-4 px-12 w-80 text-20 transition-all duration-500 hover:bg-gray-100">
+    <button className="bg-white rounded-lg font-medium py-2 lg:py-4 px-6 lg:px-12 lg:w-80 text-16 lg:text-20 transition-all duration-500 hover:bg-gray-100">
       Get Started
     </button>
   );
