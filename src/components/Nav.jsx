c761a153d7bf67ef2fb19893c29bcd09109db9f5
Orisabiyi <orisabiyidavid@gmail.com>
2024-12-11 07:27:04 +0100

    feat: optimize the navbar for mobile

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,10 +3,10 @@ import Button from "./Button";
 
 export default function Nav() {
   return (
-    <nav className="px-16 py-8 flex items-center justify-between">
+    <nav className="p-8 lg:px-16 lg:py-8 flex items-center justify-between">
       <h1 className="text-32 font-semibold">OPRA</h1>
 
-      <ul className="flex gap-10 text-16 font-medium">
+      <ul className="hidden lg:flex lg:gap-10 lg:text-16 lg:font-medium">
         <li>
           <HashLink smooth to="#why-choose-opra">
             Why Choose Opra
