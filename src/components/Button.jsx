a728d8cf8463be92b0e83a0725761ebdb0f3bff1
Orisabiyi <orisabiyidavid@gmail.com>
2024-12-11 07:33:45 +0100

    feat: validate classProp in button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,15 @@
-export default function Button() {
+import PropTypes from "prop-types";
+
+export default function Button({ classProp }) {
   return (
-    <button className="bg-white rounded-lg font-medium py-2 lg:py-4 px-6 lg:px-12 lg:w-80 text-16 lg:text-20 transition-all duration-500 hover:bg-gray-100">
+    <button
+      className={`${classProp} bg-white rounded-lg font-medium py-2 lg:py-4 px-6 lg:px-12 lg:w-80 text-16 lg:text-20 transition-all duration-500 hover:bg-gray-100`}
+    >
       Get Started
     </button>
   );
 }
+
+Button.propTypes = {
+  classProp: PropTypes.string,
+};
